4722d1eefddf9c0fe1202ef63a6262b1b46aa965
hea93fr <hea93fr@gmail.com>
2016-12-13 00:07:52 +0100

    Update test_hitcount.py
    HEA Test 12/12/16
    change : to 2 self.assertEqual(b'2', hitcount.getHit("user1"))

--- src/test_hitcount.py
+++ src/test_hitcount.py
@@ -15,7 +15,7 @@ class HitCountTest(unittest.TestCase):
         # increase the hit count for user1
         hitcount.hit("user1")
         # ensure that the hit count for user1 is just 1
-        self.assertEqual(b'1', hitcount.getHit("user1"))
+        self.assertEqual(b'2', hitcount.getHit("user1"))
 
 
 if __name__ == '__main__':
